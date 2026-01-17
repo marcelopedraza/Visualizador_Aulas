5a1b74a2cb9b64b34edb9b715d186e5f8b0eed24
marcelopedraza <marcelopedraza314@gmail.com>
2026-01-16 22:55:02 -0300

    borron y cuenta nueva

--- ejemplo.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-
-
-async def ver_cursos(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Este es un ejempplo!!')
-
-
-ejemplo_handler = CommandHandler('ejemplo', ver_cursos)
